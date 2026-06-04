1bc8ae92347717fa36a22688b579898475ac9120
ridayasth4 <ridaya350@gmail.com>
2026-06-04 19:52:12 +0545

    Switch database backend to PostgreSQL

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -77,10 +77,21 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'navyata_db',
+        'USER': 'nicecart_user',
+        'PASSWORD': 'navyata123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
