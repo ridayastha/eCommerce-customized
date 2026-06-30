bde8a17a7541f5122b2a64730650806e0fdcfb87
ridayasth4 <ridaya350@gmail.com>
2026-06-30 22:31:30 +0545

    added timeout

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -183,6 +183,7 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_TIMEOUT = 5
 
 # New setting for auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
