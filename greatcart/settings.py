108913997b1f3a3d112957bb95e93bfcd4b87d2c
ridayasth4 <ridaya350@gmail.com>
2026-06-30 14:25:30 +0545

    Fix staticfiles storage conflict with Cloudinary

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -154,6 +154,7 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
